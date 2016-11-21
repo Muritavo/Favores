0371e3c6c6626cc53cc5b5eda6e8092909990d5c
Murilo Oliveira de Araujo <muritavo@outlook.com>
2016-11-20 23:31:35 -0200

    Small fix

--- app/src/main/java/com/paladino/favores/business/UserBusiness.java
+++ app/src/main/java/com/paladino/favores/business/UserBusiness.java
@@ -19,13 +19,20 @@ import java.util.concurrent.Semaphore;
  */
 
 public class UserBusiness {
+    private static Pessoa usuario;
+
     public static Pessoa getUser() throws InterruptedException, JSONException {
-        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), null);
-        Bundle parameter = new Bundle();
-        parameter.putString("fields", "id, first_name, last_name, email, location");
-        graphRequest.setParameters(parameter);
-        JSONObject usuario = graphRequest.executeAndWait().getJSONObject();
-        Log.d("Facebook", usuario.toString());
-        return new Pessoa(usuario.getString("first_name"), usuario.getString("email"), CategoriaHorario.Tarde);
+        if (usuario == null) {
+            GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), null);
+            Bundle parameter = new Bundle();
+            parameter.putString("fields", "id, first_name, last_name, email, location");
+            graphRequest.setParameters(parameter);
+            JSONObject jsonUsuario = graphRequest.executeAndWait().getJSONObject();
+            Log.d("Facebook", jsonUsuario.toString());
+            usuario = new Pessoa(jsonUsuario.getString("first_name"), jsonUsuario.getString("email"), CategoriaHorario.Tarde);
+            return usuario;
+        } else {
+            return usuario;
+        }
     }
 }
